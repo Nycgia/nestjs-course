f3c317e65155d87023543a04b23a2ac6bd14a300
Pedro Muñoz <pmunozb@protonmail.com>
2021-01-27 10:54:56 -0300

    Valid query params

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { ETaskStatus } from '../task.model';
 
 export class GetTaskFilterDto {
+  @IsOptional()
+  @IsIn(Object.values(ETaskStatus))
   status: ETaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
