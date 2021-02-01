311f74c4eb68476e77cfc68c9c9904f24f773f80
Pedro Muñoz <pmunozb@protonmail.com>
2021-02-01 00:39:32 -0300

    Dockerfile

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: '192.168.0.5',
   port: 5432,
   username: 'postgres',
   password: 'password',
