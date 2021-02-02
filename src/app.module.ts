95aaf54e2742a6a2f9d4aa7eecdfc235aa6458ce
Pedro Muñoz <pmunozb@protonmail.com>
2021-02-02 10:43:10 -0300

    auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(TypeOrmConfig)],
+  imports: [TasksModule, TypeOrmModule.forRoot(TypeOrmConfig), AuthModule],
   controllers: [],
 })
 export class AppModule {}
