63c549dfa1703f7f66ae70a567942e94ca8314e7
Pedro Muñoz <pmunozb@protonmail.com>
2021-02-03 10:10:20 -0300

    unique user

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -8,6 +12,15 @@ export class UserRepository extends Repository<User> {
     const { username, password } = authCredentialsDto;
 
     const user = new User(username, password);
-    await user.save();
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error === '23505') {
+        throw new ConflictException('Username already exists');
+      }
+
+      throw new InternalServerErrorException(error);
+    }
   }
 }
